01723666c7b14bd1dc104fd53c31e15ce2bfc49d
saberkia <saber.kia.s@gmail.com>
2020-10-07 12:50:03 +0330

    adding name check

--- import_data.py
+++ import_data.py
@@ -1,2 +1,15 @@
 from pyedflib import highlevel
+import numpy as np
 
+
+def read_edf(edf_signal):
+    signals, signal_headers, header = highlevel.read_edf(edf_signal)
+    # signals =
+    return signals, signal_headers, header
+
+
+if __name__ == '__main__':
+    sigs, sig_headers, hdr = read_edf('Data/S001R01.edf')
+    print(sigs.shape, (sig_headers[0]['label']), type(hdr))
+    x = (sig_headers[0]['label'])
+    print(x)
