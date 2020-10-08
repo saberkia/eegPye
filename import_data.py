b22e3c576e3b735f09092a9f4379027920b2241d
saberkia <saber.kia.s@gmail.com>
2020-10-08 13:15:09 +0330

    redifinition

--- import_data.py
+++ import_data.py
@@ -1,5 +1,6 @@
 from pyedflib import highlevel
 import numpy as np
+import pandas as pd
 
 
 def read_edf(edf_signal):
@@ -10,6 +11,9 @@ def read_edf(edf_signal):
 
 if __name__ == '__main__':
     sigs, sig_headers, hdr = read_edf('Data/S001R01.edf')
+    df = pd.DataFrame(sig_headers)
     print(sigs.shape, (sig_headers[0]['label']), type(hdr))
     x = (sig_headers[0]['label'])
     print(x)
+    print(df.head())
+    print(df['sample_rate'])
