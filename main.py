35e55476ee776919ac00f4260df4c941411c28cb
saberkia <saber.kia.s@gmail.com>
2020-10-09 14:19:15 +0330

    lastest Changes for test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
 from eeg_signal import EEG
 from read_eeg import read_edf
 
-eeg_sign, eeg_hdr = read_edf('Data/S001R01.edf')
-eeg = EEG(eeg_sign, eeg_hdr)
+eeg_signal, eeg_hdr = read_edf('Data/S001R01.edf')
+eeg = EEG(eeg_signal, eeg_hdr)
