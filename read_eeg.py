350035446d132ce5b0f46e14740b9c212a6869fa
saberkia <saber.kia.s@gmail.com>
2020-10-14 09:56:38 +0330

    adding support for mat, pickle, hdf5 & CSV files

--- read_eeg.py
+++ read_eeg.py
@@ -1,7 +1,9 @@
 from pyedflib import highlevel
 import numpy as np
 import pandas as pd
-
+import scipy.io
+import pickle
+import h5py
 
 def read_edf(edf_file):
     signal, signal_headers, header = highlevel.read_edf(edf_file)
@@ -9,19 +11,38 @@ def read_edf(edf_file):
 
 def read_txt(txt_file):
     file = open(txt_file, mode='r')
-    str = file.read()
+    str_ = file.read()
     file.close()
-    str = str.translate({ord('\n'): None , ord(' '): None})
-    signal_str = str.split(sep=',', maxsplit=-1)
+    new_str = str_.translate({ord('\n'): None, ord(' '): None})
+    signal_str = new_str.split(sep=',', maxsplit=-1)
     signal = np.zeros(len(signal_str))
     for index in enumerate(signal_str):
         signal[index[0]] = np.double(signal_str[index[0]])
     return signal
+
+def read_csv(csv_file):
+    signal = pd.read_csv(csv_file)
+    return signal
+
+def raed_mat(mat_file):
+    signal = scipy.io.loadmat(mat_file)
+    return signal
+
+def read_pickle(pickle_file):
+    with open(pickle_file, 'rb') as file:
+        signal = pickle.load(file)
+    return signal
+
+def read_hdf5(hdf5_file):
+    signal = h5py.File(hdf5_file, 'r')
+    return signal
+
 # TODO :
-#       Addind Support for csv file
-#       Addind Support for mat file
-#       Addind Support for pickle file
-#       Addind Support for hdf5 file
+#       Adding Support for csv file     : done
+#       Adding Support for mat file     : done
+#       Adding Support for pickle file  : done
+#       Adding Support for hdf5 file    : done
+
 
 if __name__ == '__main__':
     sigs, sig_headers = read_edf('Data/S001R01.edf')
